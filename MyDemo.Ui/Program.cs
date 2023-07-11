5064448b56ddb7ae0aacc60ccf9a948376cda7d2
Patrick Drechsler <patrick@pdrechsler.de>
2023-07-11 23:34:23 +0200

    up

--- MyDemo.Ui/Program.cs
+++ MyDemo.Ui/Program.cs
@@ -1,13 +1,26 @@
 ﻿using MyDemo.Ui;
 using Terminal.Gui;
 
-Application.Init();
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Application.Init();
 
-try
-{
-    Application.Run(new MyView());
-}
-finally
-{
-    Application.Shutdown();
+        var top = Application.Top;
+
+        var login = new Label("Login: ") { X = 0, Y = 0 };
+        top.Add(login);
+
+        var win = new Window("My Demo")
+        {
+            X = 0,
+            Y = 1,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+
+        top.Add(win);
+        Application.Run();
+    }
 }
